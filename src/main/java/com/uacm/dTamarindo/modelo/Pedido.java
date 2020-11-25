e5219858183ab397e019e283aa3b010b606515b9
Ingrid <ingrid.casales@tecno.mx>
2020-11-25 03:10:19 -0600

    Clases de dominio
    Se agregan clases de dominio y y nueva dependencia

--- src/main/java/com/uacm/dTamarindo/modelo/Pedido.java
+++ src/main/java/com/uacm/dTamarindo/modelo/Pedido.java
@@ -1,8 +1,32 @@
 package com.uacm.dTamarindo.modelo;
 
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Pedido {
-	private int id;
-	private String fecha;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@ManyToOne
+	private Usuario idComercializadora;
+	
+	@NotNull
+	private Date fecha;
 	
 }
--- src/main/java/com/uacm/dTamarindo/modelo/Producto.java
+++ src/main/java/com/uacm/dTamarindo/modelo/Producto.java
@@ -1,10 +1,38 @@
 package com.uacm.dTamarindo.modelo;
 
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Producto {
 	
-	private int id;
-	private int piezas;
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@NotNull
+    private int piezas;
+	
+
+	@NotNull
+    @Size(max = 100)
 	private String nombre;
+	
+	@NotNull
+    @Size(max = 200)
 	private String descripcion;
+	
 	
 }
